521d28bf810b566a2d7b001a4d29aaf9408bce8b
战虎 <earthvillage.top@outlook.com>
2025-04-03 23:04:58 +0800

    s哈巴

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,7 +1,7 @@
 import { basename } from 'node:path'
 import { defineConfig } from 'vitepress'
 import MarkdownPreview from 'vite-plugin-markdown-preview'
-import Markdown from 'vite-plugin-md' // 新增导入
+
 import { head, nav, sidebar } from './configs'
 
 const APP_BASE_PATH = basename(process.env.GITHUB_REPOSITORY || '')
@@ -75,17 +75,5 @@ export default defineConfig({
       category: 'Announcements',
       categoryId: 'DIC_kwDONdxk_84CmNH4',
     },
-  },
-
-  vite: {
-    plugins: [
-      MarkdownPreview(),
-      Markdown({ // 配置 Markdown 插件
-        markdownItOptions: {
-          html: true,     // 允许 HTML 标签
-          xhtmlOut: false // 不强制闭合标签
-        }
-      })
-    ]
   }
 })
