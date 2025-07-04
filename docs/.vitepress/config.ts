e886c99ff55d7c53e7dbdb3a86521dbba51599df
战虎 <earthvillage.top@outlook.com>
2025-07-04 10:57:20 +0800

    傻逼检查

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -7,6 +7,8 @@ import { head, nav, sidebar } from './configs'
 const APP_BASE_PATH = basename(process.env.GITHUB_REPOSITORY || '')
 
 export default defineConfig({
+    ignoreDeadLinks: true, // 禁用死链检查
+
   outDir: '../dist',
   // base: APP_BASE_PATH ? `/${APP_BASE_PATH}/` : '/',
   base: '/',
